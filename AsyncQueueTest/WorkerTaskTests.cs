3be1161798c24a23921c3898628285af98800bb4
Sunlighter <edkiser@gmail.com>
2017-06-17 13:39:17 -0700

    Add more tests

--- AsyncQueueTest/WorkerTaskTests.cs
+++ AsyncQueueTest/WorkerTaskTests.cs
@@ -25,7 +25,7 @@ namespace Sunlighter.AsyncQueueTest
             {
                 for (int i = 0; i < 100; ++i)
                 {
-                    await q1.Enqueue(r.Next(1000), CancellationToken.None);
+                    await q1.Enqueue(r.Next(1000), ec.CancellationToken);
                 }
                 q1.WriteEof();
             };
@@ -37,7 +37,7 @@ namespace Sunlighter.AsyncQueueTest
                 q1,
                 async delegate (ForEachInfo<int> fi)
                 {
-                    await q2.Enqueue(fi.Item * 100, CancellationToken.None);
+                    await q2.Enqueue(fi.Item * 100, fi.CancellationToken);
                 },
                 delegate ()
                 {
@@ -137,6 +137,149 @@ namespace Sunlighter.AsyncQueueTest
                 ec.WaitAll(T1, T2, T3);
             }
             catch(Exception exc)
+            {
+                System.Diagnostics.Debug.WriteLine(exc);
+                threwException = true;
+            }
+
+            Assert.IsTrue(threwException);
+        }
+
+        [TestMethod]
+        public void ParallelForEachTest()
+        {
+            object rSyncRoot = new object();
+            Random r = new Random((int)((System.Diagnostics.Stopwatch.GetTimestamp() >> 3) & 0x7FFFFFFF));
+
+            ExceptionCollector ec = new ExceptionCollector();
+
+            AsyncQueue<int> q1 = new AsyncQueue<int>(5);
+
+            Func<Task> t1 = async delegate ()
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    await q1.Enqueue(r.Next(1000), ec.CancellationToken);
+                }
+                q1.WriteEof();
+            };
+
+            AsyncQueue<int> q2 = new AsyncQueue<int>(5);
+
+            ParallelWorker pw = new ParallelWorker(8);
+
+            Func<Task> t2 = WorkerTask.ParallelForEach
+            (
+                q1,
+                pw,
+                async delegate (ForEachInfo<int> fi)
+                {
+                    await q2.Enqueue(fi.Item * 100, fi.CancellationToken);
+
+                    int time;
+                    lock (rSyncRoot)
+                    {
+                        time = 100 + r.Next(800);
+                    }
+                    await Task.Delay(time);
+                },
+                delegate ()
+                {
+                    q2.WriteEof();
+                    return Task.CompletedTask;
+                },
+                ec
+            );
+
+            List<int> items = new List<int>();
+
+            Func<Task> t3 = WorkerTask.ForEach
+            (
+                q2,
+                delegate (ForEachInfo<int> fi)
+                {
+                    items.Add(fi.Item);
+                    System.Diagnostics.Debug.WriteLine(fi.Item);
+                    return Task.CompletedTask;
+                },
+                delegate ()
+                {
+                    return Task.CompletedTask;
+                },
+                ec
+            );
+
+            Task T1 = Task.Run(t1);
+            Task T2 = Task.Run(t2);
+            Task T3 = Task.Run(t3);
+
+            Task.WaitAll(T1, T2, T3);
+
+            Assert.AreEqual(0, ec.Exceptions.Count);
+            Assert.AreEqual(100, items.Count);
+        }
+
+        [TestMethod]
+        public void ParallelForEachExceptionTest()
+        {
+            ExceptionCollector ec = new ExceptionCollector();
+
+            AsyncQueue<int> q1 = new AsyncQueue<int>(5);
+
+            Func<Task> t1 = async delegate ()
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    await q1.Enqueue(i, ec.CancellationToken);
+                }
+                q1.WriteEof();
+            };
+
+            AsyncQueue<int> q2 = new AsyncQueue<int>(5);
+
+            Func<Task> t2 = WorkerTask.ForEach
+            (
+                q1,
+                async delegate (ForEachInfo<int> fi)
+                {
+                    await q2.Enqueue(fi.Item * 100, fi.CancellationToken);
+                    if (fi.Item == 53) throw new InvalidOperationException("test 1");
+                },
+                delegate ()
+                {
+                    q2.WriteEof();
+                    return Task.CompletedTask;
+                },
+                ec
+            );
+
+            List<int> items = new List<int>();
+
+            Func<Task> t3 = WorkerTask.ForEach
+            (
+                q2,
+                delegate (ForEachInfo<int> fi)
+                {
+                    items.Add(fi.Item);
+                    return Task.CompletedTask;
+                },
+                delegate ()
+                {
+                    return Task.CompletedTask;
+                },
+                ec
+            );
+
+            Task T1 = Task.Run(t1);
+            Task T2 = Task.Run(t2);
+            Task T3 = Task.Run(t3);
+
+            bool threwException = false;
+            try
+            {
+                ec.WaitAll(T1, T2, T3);
+            }
+            catch (Exception exc)
             {
                 System.Diagnostics.Debug.WriteLine(exc);
                 threwException = true;
